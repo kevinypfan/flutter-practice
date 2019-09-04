d32f86d2022dac7902a37f94afe77ad8214c541a
KevinFan <kevinypfan@gmail.com>
2019-09-05 00:42:58 +0800

    max udemy course: 251, OrdersScreen Widget convert to StatelessWidget

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,29 +5,8 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  // var _isLoading = false;
-
-  @override
-  void initState() {
-    // Future.delayed(Duration.zero).then((_) async {
-    //   setState(() {
-    //     _isLoading = true;
-    //   });
-    //   await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-    //   setState(() {
-    //     _isLoading = false;
-    //   });
-    // });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
